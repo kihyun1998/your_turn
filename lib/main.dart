fdbecc84f6bf5453605195505712293ba14265f6
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2024-03-16 17:09:12 +0900

    리팩토링

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:your_turn/firebase_options.dart';
+import 'package:your_turn/util/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
